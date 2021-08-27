c0669250091aabc0bbef51906f9f84adbdc8cac6
jubakong <jubakong@gmail.com>
2021-08-27 23:53:05 +0900

    Add 'JBTextView.swift'

---
+++ JBTextView/JBTextView.swift
@@ -0,0 +1,12 @@
+//
+//  JBTextView.swift
+//  JBTextView
+//
+//  Created by Jeongbae Kong on 2021/08/27.
+//
+
+import UIKit
+
+class JBTextView: UITextView {
+  
+}
---
+++ JBTextView/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  JBTextView
+//
+//  Created by Jeongbae Kong on 2021/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
